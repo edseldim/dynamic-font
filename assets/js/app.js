90262d3de5582e35df08cd9934708dd9cde166a0
edseldim <edselmanuel@hotmail.com>
2022-12-11 00:52:39 -0500

    added clickable header btns

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,8 @@ function registerListeners(){
         toggleBtn.innerHTML = '<svg height=20 width=20 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" /> </svg>'
     });
 
-    contactBtn.addEventListener("click", () =>{
+    contactBtn.addEventListener("click", (e) =>{
+        e.preventDefault();
         const modalBlock = document.createElement("DIV");
         modalBlock.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
         modalBlock.style.top = "0";
